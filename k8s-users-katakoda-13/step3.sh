b9a1810b9fc52f2f5baecbcfe945c9855e5a5388
Frederic Leger <frederic@webofmars.com>
2018-09-30 15:01:38 +0200

    nicer output

--- k8s-users-katakoda-13/step3.sh
+++ k8s-users-katakoda-13/step3.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set +x
+
 echo -n 'Waiting after kubernetes spin-up '
-until [ "$(kubectl get nodes |tail -1 |awk '{ print $2 }')" = "Ready" ]; do
+until [ "$(kubectl get nodes 2>/dev/null |tail -1 |awk '{ print $2 }')" = "Ready" ]; do
     echo -n '.'
     sleep 3
 done
 echo ' \o/'
-
