3fbade565c1facced507a52861efc1aafd7da6d6
Frederic Leger <frederic@webofmars.com>
2019-04-18 22:49:21 +0200

    FIX: missing assets

--- k8s-users-katacoda-14/intro.sh
+++ k8s-users-katacoda-14/intro.sh
@@ -3,9 +3,15 @@
 set -e -o pipefail
 exec > intro.log 2>&1
 
-echo "+ unzipping ressources"
+echo "+ get scenario ressources"
 wget 'https://s3.eu-west-3.amazonaws.com/com.webofmars.public/formations/katacoda/helm/sf4-animals-demo.tgz'
-tar -zxvf sf4-animals-demo.tgz
+tar -zxvf sf4-animals-demo.tgz && rm sf4-animals-demo.tgz
+
+echo "+ wait for kube"
+until [ "$(kubectl get nodes 2>/dev/null |tail -1 |awk '{ print $2 }')" = "Ready" ]; do
+    echo -n '.'
+    sleep 3
+done
 
 echo "+ finished succesfuly"
 exit 1
